58a9550cf938045a2619a85e782e7737c3fcaeed
Svetlin Totlyakov <Svetlin.Totlyakov@sage.com>
2021-06-07 15:15:47 -0700

    again

--- startup.sh
+++ startup.sh
@@ -4,4 +4,5 @@
 #apt-get install -y libglib2.0-0
 #apt-get install -y libxrender-dev
 apt-get install -y python3-opencv
+pip install opencv-python
 gunicorn --bind=0.0.0.0 --timeout 600 index:server
