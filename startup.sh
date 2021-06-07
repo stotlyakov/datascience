1e6b254e6be3757661b6277f0f9434cffe00b67b
Svetlin Totlyakov <Svetlin.Totlyakov@sage.com>
2021-06-07 16:39:18 -0700

    again

--- startup.sh
+++ startup.sh
@@ -3,6 +3,7 @@
 #apt-get install libgtk2.0-dev
 #apt-get install -y libglib2.0-0
 #apt-get install -y libxrender-dev
-apt-get install -y python3-opencv
+apt-get install ffmpeg libsm6 libxext6  -y
+#apt-get install -y python3-opencv
 pip install opencv-python
 gunicorn --bind=0.0.0.0 --timeout 600 index:server
