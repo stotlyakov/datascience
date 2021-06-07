7539ed2d8f28068da7d451a8a5fccd9216c8a8b8
Svetlin Totlyakov <Svetlin.Totlyakov@sage.com>
2021-06-07 14:34:25 -0700

    and again

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get install libgtk2.0-dev
+apt-get install -y libglib2.0-0
 apt install -y libsm6 libxext6
 apt-get install -y libxrender-dev
 gunicorn --bind=0.0.0.0 --timeout 600 index:server
