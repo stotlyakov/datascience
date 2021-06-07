8969d3edc5a6805d87e0ef73657adb40784f103a
Svetlin Totlyakov <Svetlin.Totlyakov@sage.com>
2021-06-07 15:07:19 -0700

    more

--- startup.sh
+++ startup.sh
@@ -3,4 +3,5 @@
 #apt-get install libgtk2.0-dev
 #apt-get install -y libglib2.0-0
 #apt-get install -y libxrender-dev
+apt-get install -y python3-opencv
 gunicorn --bind=0.0.0.0 --timeout 600 index:server
